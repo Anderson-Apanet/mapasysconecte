ff0e70d6c3d71a0191063e9e442f38cc2dc54ea6
Anderson-Apanet <contato@apanet.info>
2024-12-30 19:01:50 -0300

    perf: Otimizando chunks do build para melhor performance

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,20 @@ export default defineConfig({
       }
     }
   },
+  build: {
+    chunkSizeWarningLimit: 1000, // Aumentando o limite de tamanho dos chunks
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Separando as dependências em chunks diferentes
+          vendor: ['react', 'react-dom'],
+          maps: ['@react-google-maps/api'],
+          ui: ['@material-tailwind/react', '@headlessui/react', '@heroicons/react'],
+          calendar: ['@fullcalendar/core', '@fullcalendar/daygrid', '@fullcalendar/timegrid', '@fullcalendar/list', '@fullcalendar/interaction']
+        }
+      }
+    }
+  },
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
     'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || ''),
